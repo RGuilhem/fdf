0c369331ce1079a8f0eab65ebc338b9dae6eca54
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-02-07 15:57:30 +0100

    took rapid put_pixel form old fdf

---
+++ src/put_pixel.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_pixel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: graux <marvin@42lausanne.ch>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 15:36:26 by graux             #+#    #+#             */
+/*   Updated: 2023/02/07 15:08:21 by graux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+#include "../includes/mlx.h"
+#include "../includes/_frame.h"
+
+void	put_pixel(t_frame *frame, int x, int y, int color)
+{
+	char			*pix;
+	t_priv_frame	*fra;
+
+	pix = fra->address + (y * fra->line_length + x * (fra->bits_per_pixel / 8));
+	*(unsigned int *) pix = color;
+}
