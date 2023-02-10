291421a7dc782c99b09765cf7cc7aac84ef67766
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-02-10 16:37:41 +0100

    vector creation and destruction

--- src/vec_destroy.c
+++ src/vec_destroy.c
@@ -1,29 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vec_create.c                                       :+:      :+:    :+:   */
+/*   vec_destroy.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/10 15:16:16 by graux             #+#    #+#             */
-/*   Updated: 2023/02/10 16:34:34 by graux            ###   ########.fr       */
+/*   Created: 2023/02/10 16:35:20 by graux             #+#    #+#             */
+/*   Updated: 2023/02/10 16:36:21 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
-#include "../includes/_vec.h"
-#include "../includes/error.h"
 #include <stdlib.h>
 
-t_vec	*vec_create(float x, float y, float z)
+void	vec_destroy(t_vec *vec)
 {
-	t_vec_	*new_vec;
-
-	new_vec = malloc(sizeof(t_vec_));
-	if (!new_vec)
-		error_handler(CRASH, NULL, NULL);
-	new_vec->x = x;
-	new_vec->y = y;
-	new_vec->z = z;
-	return (new_vec);
+	free(vec);
 }
