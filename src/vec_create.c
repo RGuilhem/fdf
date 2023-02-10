4dc4a9ea752d673015a1e5f948ada175864de845
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-02-10 15:19:34 +0100

    empty vec_create: first do err handling

---
+++ src/vec_create.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vec_create.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 15:16:16 by graux             #+#    #+#             */
+/*   Updated: 2023/02/10 15:18:35 by graux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+#include "../includes/_vec.h"
+
+t_vec	*vec_create(float x, float y, float z)
+{
+	t_vec_	*new_vec;
+
+	//TODO implement
+	return (new_vec);
+}
