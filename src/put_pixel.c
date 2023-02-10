b60f2601d95182d23d9f6a742a469ce7895bbfd3
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-02-10 13:22:34 +0100

    correction to use new ADT for frame

--- src/put_pixel.c
+++ src/put_pixel.c
@@ -6,7 +6,7 @@
 /*   By: graux <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 15:36:26 by graux             #+#    #+#             */
-/*   Updated: 2023/02/07 15:08:21 by graux            ###   ########.fr       */
+/*   Updated: 2023/02/10 12:50:17 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	put_pixel(t_frame *frame, int x, int y, int color)
 	char			*pix;
 	t_priv_frame	*fra;
 
+	fra = (t_priv_frame *) frame;
 	pix = fra->address + (y * fra->line_length + x * (fra->bits_per_pixel / 8));
 	*(unsigned int *) pix = color;
 }
