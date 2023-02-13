4b641a94fcfce34b5b40eeeaf6cdb21b30b5240d
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-02-13 14:15:36 +0100

    matrices work, noice

--- src/main.c
+++ src/main.c
@@ -6,15 +6,31 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 14:55:29 by graux             #+#    #+#             */
-/*   Updated: 2023/02/07 14:59:09 by graux            ###   ########.fr       */
+/*   Updated: 2023/02/13 13:24:46 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+#include "../includes/_vec.h" //TODO remove
+#include "../includes/_matrix.h" //TODO remove
+#include <stdio.h>            //TODO remove
+#include <math.h>
 
 int	main(int argc, char *argv[])
 {
+	t_vec		*vec;
+	t_vec_		*real;
+	t_matrix	*matrix;
+
 	(void) argc;
 	(void) argv;
+	vec = vec_create(1, 0, 0);
+	real = (t_vec_ *) vec;
+	printf("before: (%f, %f, %f)\n", real->x, real->y, real->z);
+	matrix = matrix_rotz_create(1.5708);
+	vec_multiply(vec, matrix);
+	printf("after:  (%f, %f, %f)\n", real->x, real->y, real->z);
+	vec_multiply(vec, matrix);
+	printf("two times:  (%f, %f, %f)\n", real->x, real->y, real->z);
 	return (0);
 }
--- src/matrix_create.c
+++ src/matrix_create.c
@@ -6,18 +6,20 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 10:56:54 by graux             #+#    #+#             */
-/*   Updated: 2023/02/13 11:03:00 by graux            ###   ########.fr       */
+/*   Updated: 2023/02/13 13:08:03 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 #include "../includes/_matrix.h"
 #include <math.h>
+#include <stdlib.h>
 
 t_matrix	*matrix_rotx_create(float a)
 {
 	t_matrix_	*matrix;
 
+	matrix = malloc(sizeof(t_matrix_));
 	matrix->row0 = vec_create(1, 0, 0);
 	matrix->row1 = vec_create(0, cos(a), -sin(a));
 	matrix->row2 = vec_create(0, sin(a), cos(a));
@@ -28,6 +30,7 @@ t_matrix	*matrix_roty_create(float a)
 {
 	t_matrix_	*matrix;
 
+	matrix = malloc(sizeof(t_matrix_));
 	matrix->row0 = vec_create(cos(a), 0, sin(a));
 	matrix->row1 = vec_create(0, 1, 0);
 	matrix->row2 = vec_create(-sin(a), 0, cos(a));
@@ -38,6 +41,7 @@ t_matrix	*matrix_rotz_create(float a)
 {
 	t_matrix_	*matrix;
 
+	matrix = malloc(sizeof(t_matrix_));
 	matrix->row0 = vec_create(cos(a), -sin(a), 0);
 	matrix->row1 = vec_create(sin(a), cos(a), 0);
 	matrix->row2 = vec_create(0, 0, 1);
