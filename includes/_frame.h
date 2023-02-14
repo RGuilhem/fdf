05e0ff9bc7fff4140a3299193d176df4d2c97d90
Guilhem Raux <graux@c1r6s1.42lausanne.ch>
2023-02-14 11:21:42 +0100

    naming from s_priv_frame -> s_frame_

--- includes/_frame.h
+++ includes/_frame.h
@@ -6,20 +6,20 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:04:35 by graux             #+#    #+#             */
-/*   Updated: 2023/02/07 15:06:41 by graux            ###   ########.fr       */
+/*   Updated: 2023/02/14 11:21:02 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _FRAME_H
 # define _FRAME_H
 
-typedef struct s_priv_frame
+typedef struct s_frame_
 {
 	void	*img;
 	char	*address;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}				t_priv_frame;
+}				t_frame_;
 
 #endif
